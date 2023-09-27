365ad2d4edb84410590b504fb6b5d03ad86adb8b
Denzel Gitonga <gitongadenzel@gmail.com>
2023-09-27 13:33:43 +0300

    Models

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+# Import models
+from .models import About
 # Register your models here.
+admin.site.register(About)
--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(null=True)
+
+    """ Stingify the fields name """
+    def __str__(self):
+        return self.name
+
