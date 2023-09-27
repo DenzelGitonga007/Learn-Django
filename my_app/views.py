9f485e4493be722ef442407c64baf68886a66b81
Denzel Gitonga <gitongadenzel@gmail.com>
2023-09-27 13:42:24 +0300

    Views

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+# Import views
+from .models import About
 # Create your views here.
+
+""" CRUD functionality
+    C - Create
+    R - Read
+    U - Update
+    D - Delete
+"""
+
+# Read
+def read(request):
+    # Get all abouts in the db
+    abouts = About.objects.all()
+    context = { 'abouts': abouts } # Map each about to a key, as a key-value pair dictionary
+    return render(request, 'my_app/list_abouts.html', context)
+
