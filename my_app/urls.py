e04c0345b364c4bf44c9f04c2f547f578b6f7cca
Denzel Gitonga <gitongadenzel@gmail.com>
2023-10-11 16:08:22 +0300

    read item details

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'my_app'
 
 urlpatterns = [
-    path('list/', views.list, name="list_abouts")
+    path('list/', views.list, name="list_abouts"), # list all items
+    path('item/<int:item_id>/details', views.read, name="item_details"), # view item details
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Import views
 from .models import About
 # Create your views here.
@@ -17,3 +17,8 @@ def list(request):
     context = { 'abouts': abouts } # Map each about to a key, as a key-value pair dictionary
     return render(request, 'my_app/list_abouts.html', context)
 
+# Read
+def read(request, item_id):
+    item = get_object_or_404(About, pk=item_id) # get the item by id
+    context = { 'item': item }
+    return render(request, 'my_app/item_details.html', context)
